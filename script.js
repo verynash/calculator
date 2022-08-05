7f287370248b597293f99bb3cc071b09fe306a8e
verynash <sbz.chary@gmail.com>
2022-08-05 12:53:34 -0400

    Define buttons in script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const displayValue = document.querySelector('.display');
+let a = 0;
+let b = 0;
+
+const btnOne = document.getElementById('one');
+const btnTwo = document.getElementById('two');
+const btnThree = document.getElementById('three');
+const btnFour = document.getElementById('four');
+const btnFive = document.getElementById('five');
+const btnSix = document.getElementById('six');
+const btnSeven = document.getElementById('seven');
+const btnEight = document.getElementById('eight');
+const btnNine = document.getElementById('nine');
+const btnZero = document.getElementById('zero');
+const btnClear = document.getElementById('clear');
+const btnDecimal = document.getElementById('decimal');
+const btnDivide = document.getElementById('divide');
+const btnMultiply = document.getElementById('multiply');
+const btnSubtract = document.getElementById('subtract');
+const btnAdd = document.getElementById('add');
+const btnOperate = document.getElementById('operate');
+
+
+
+
+
 function operate(operator,a,b) {
 
 }
