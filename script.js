cf8fe7ec97bc6c3d29fc71d095aeae32e4157a11
verynash <sbz.chary@gmail.com>
2022-08-08 11:34:22 -0400

    Add updateDisplay() and inputValue() and HTML values

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
-const displayValue = document.querySelector('.display');
-let a = 0;
-let b = 0;
-
-const btnOne = document.getElementById('one');
-const btnTwo = document.getElementById('two');
-const btnThree = document.getElementById('three');
-const btnFour = document.getElementById('four');
-const btnFive = document.getElementById('five');
-const btnSix = document.getElementById('six');
-const btnSeven = document.getElementById('seven');
-const btnEight = document.getElementById('eight');
-const btnNine = document.getElementById('nine');
-const btnZero = document.getElementById('zero');
-const btnClear = document.getElementById('clear');
-const btnDecimal = document.getElementById('decimal');
-const btnDivide = document.getElementById('divide');
-const btnMultiply = document.getElementById('multiply');
-const btnSubtract = document.getElementById('subtract');
-const btnAdd = document.getElementById('add');
-const btnOperate = document.getElementById('operate');
-
-
-
-
-
-function operate(operator,a,b) {
-
+const calculator = {
+    displayValue: '0',
+    firstNumber: null,
+    waitingForSecondOperand: false,
+    operator: null
 }
 
-function add(a,b) {
+function updateDisplay() {
+    let display = document.getElementById('display');
+    display.textContent = calculator.displayValue;
+}
 
+updateDisplay();
+
+const buttons = document.querySelectorAll('button');
+for (const button of buttons) {
+    button.addEventListener('click', (event) => {
+        const target = event.target;
+        if (!target.matches('button')) {
+            return;
+        }
+        if (target.classList.contains('number-btn')) {
+            console.log('digit', target.value)
+            inputValue(target.value)
+            updateDisplay()
+        }
+        if (target.classList.contains('operator-btn')) {
+            console.log('operator', target.value)
+        } 
+        if (target.classList.contains('operate-btn')) {
+            console.log('operate', target.value)
+        }
+    });
+}
+
+function inputValue(number) {
+    let display = calculator.displayValue;
+    calculator.displayValue = display === '0' ? number : display + number;
+}
+
+
+
+
+
+
+
+
+
+
+
+function add(a,b) {
+    total = a + b;
+    return total;
 }
 
 function subtract(a,b) {
-
+    total = a - b;
+    return total;
 }
 
 function multiply(a,b) {
-
+    total = a * b;
+    return total;
 }
 
 function divide(a,b) {
-
+    total = a / b;
+    return total;
 }
